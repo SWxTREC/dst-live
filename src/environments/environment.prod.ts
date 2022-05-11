d7e9f09a1a2adb60f4f52fbd35bda93bf6ac137a
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2022-05-11 15:50:14 -0600

    WIP add gh-pages and scripts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,13 @@ import { version } from '../../package.json';
 export const environment = {
     production: true,
     latisSwp: 'https://lasp.colorado.edu/space-weather-portal/latis/dap/',
+    mockData1hr: 'assets/data/mockData_1hr.json',
+    mockData2hr: 'assets/data/mockData_2hr.json',
+    mockData3hr: 'assets/data/mockData_3hr.json',
+    mockData4hr: 'assets/data/mockData_4hr.json',
+    mockData5hr: 'assets/data/mockData_5hr.json',
+    mockData6hr: 'assets/data/mockData_6hr.json',
+    mockData7hr: 'assets/data/mockData_7hr.json',
+    mockDataFuture: 'assets/data/mockData_future.json',
     version: version
 };
